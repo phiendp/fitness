9aff58e998890da22cc10d7d164a9e3383d54a98
Phien Pham <phien@triip.me>
2017-12-03 09:40:31 +0700

    Add Slider and Steppers components to AddEntry

--- components/AddEntry.js
+++ components/AddEntry.js
@@ -1,6 +1,9 @@
 import React, { Component } from 'react'
 import { View } from 'react-native'
 import { getMetricMetaInfo } from '../utils/helpers'
+import Slider from './Slider'
+import Steppers from './Steppers'
+
 
 export default class AddEntry extends Component {
   state = {
@@ -40,9 +43,32 @@ export default class AddEntry extends Component {
   }
 
   render() {
+    const metaInfo = getMetricMetaInfo()
+
     return (
       <View>
-        {getMetricMetaInfo('bike').getIcon()}
+        {Object.keys(metaInfo).map((key) => {
+          const { getIcon, type, ...rest } = metaInfo[key]
+          const value = this.state[key]
+
+          return (
+            <View key = {key}>
+              {getIcon()}
+              {type === 'slider'
+                ? <Slider 
+                    value={value}
+                    onChange={(value) => this.slide(key, value)}
+                    {...rest}
+                  />
+                : <Steppers 
+                    value={value}
+                    onIncrement={() => this.increment(key)}
+                    onDecrement={() => this.decrement(key)}
+                    {...rest}
+                  />}
+            </View>
+          )
+        })}
       </View>
     )
   }
