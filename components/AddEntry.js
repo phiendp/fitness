4adcb64461e4ea205fd0ce4794434f3b4d663138
Phien Pham <phien@triip.me>
2017-12-04 12:05:18 +0700

    Init Submit btn

--- components/AddEntry.js
+++ components/AddEntry.js
@@ -1,10 +1,19 @@
 import React, { Component } from 'react'
-import { View } from 'react-native'
-import { getMetricMetaInfo } from '../utils/helpers'
+import { View, TouchableOpacity, Text } from 'react-native'
+import { getMetricMetaInfo, timeToString } from '../utils/helpers'
 import Slider from './Slider'
 import Steppers from './Steppers'
 import DateHeader from './DateHeader'
 
+
+function SubmitBtn({ onPress }) {
+  return (
+    <TouchableOpacity
+      onPress={onPress}>
+        <Text> Submit </Text>
+    </TouchableOpacity>
+  )
+}
 
 export default class AddEntry extends Component {
   state = {
@@ -43,6 +52,19 @@ export default class AddEntry extends Component {
     }))
   }
 
+  submit = () => {
+    const key = timeToString()
+    const entry = this.state
+
+    this.setState(() => ({
+      run: 0,
+      bike: 0,
+      swin: 0,
+      sleep: 0,
+      eat: 0,
+    }))
+  }
+
   render() {
     const metaInfo = getMetricMetaInfo()
 
@@ -71,6 +93,7 @@ export default class AddEntry extends Component {
             </View>
           )
         })}
+        <SubmitBtn onPress={this.submit} />
       </View>
     )
   }
